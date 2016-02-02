782f14b6782dd97ce8ab0e396d17b7d7abdddf76
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2016-02-02 16:09:23 +0530

    testing code

---
+++ NG_CtrlTestApp/MyScripts/app.js
@@ -0,0 +1,17 @@
+﻿ //1.
+var app = app = angular.module('appmodule', []);
+//2.
+app.controller('mycontroller', function ($scope) {
+    $scope.Employee = {
+        EmpNo: 0,
+        EmpName: "",
+    };
+    //3.
+    $scope.Employee.EmpName = 'MS';
+
+    //4.
+    $scope.lower = function (ename) {
+        return ename.toLowerCase(); 
+    };
+     
+});
---
+++ NG_CtrlTestApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NG_CtrlTestApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NG_CtrlTestApp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("229e9479-370e-4e28-b8f2-983b05afd944")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
